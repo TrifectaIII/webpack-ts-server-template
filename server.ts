1f77dbbc318820b90384a6f7f693c35604d0d830
Dakota Madden-Fong <maddenfong@gmail.com>
2021-10-18 09:45:16 -0700

    added sqlite

--- server.ts
+++ server.ts
@@ -1,8 +1,41 @@
 import * as path from 'path';
 import * as express from 'express';
+import * as sqlite3 from 'sqlite3';
 
-const app = express(); //create express app
-const port: number = 3000;
+const port: number = 3000; //port for listening
+
+const app: express.Express = express(); //create express app
+
+const db: sqlite3.Database = new sqlite3.Database(':memory:'); //create in-memory db
+
+//setup db
+db.serialize(() => {
+
+    //create table
+    db.run(`CREATE TABLE users (
+        user_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    )`);
+
+    //fill table with data
+    const statement: sqlite3.Statement = db.prepare(`INSERT INTO users (name) VALUES (?)`);
+    const names: string[] = ['Mark', 'Stephanie', 'Julia'];
+    for (let i=0; i<names.length; i++) {
+        statement.run(names[i]);
+    }
+    statement.finalize();
+
+    //retrieve data
+    db.all(`SELECT user_id, name FROM users`, (err, rows) => {
+        // console.log(rows);
+    });
+    
+});
+
+//basic get route
+app.get('/getreq', (req, res) => {
+    res.send('Hello!');
+});
 
 // serve static files from build folder
 app.use('/', express.static(path.resolve('./build')));
